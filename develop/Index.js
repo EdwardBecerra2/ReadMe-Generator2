c9803b656cd23cf587a5c452e94e04008d782bfd
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-01-16 11:40:14 -0800

    github username question

--- develop/Index.js
+++ develop/Index.js
@@ -2,9 +2,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
  
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github username',
+        Validate: githubInput => {
+            if(githubInput){
+                return true;
+            } else {
+                console.log('Please enter your Github username!');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
