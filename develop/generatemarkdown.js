893ec2174bfde7c0319e3bcb56ca55c21fefa8a1
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-01-18 11:35:23 -0800

    gen markdown complete, trouble making readme

--- develop/generatemarkdown.js
+++ develop/generatemarkdown.js
@@ -16,12 +16,51 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return ` 
+    ### Licensing 
+    ${license} 
+    ${renderLicenseBadge(license)}`
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  //console.log(data)
+  const { github, badgeLabel, linkUrl, ...info} = data;
   return `# ${data.title}
 
+  ## Table of Contents
+  - [Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+  ## Project Description
+  ${info.description}
+  ${renderLicenseBadge(badgeLabel)} 
+  ${renderLicenseLink(linkUrl)}
+  ${renderLicenseSection()}
+
+  ## Installation 
+  ${info.installationInstructions}
+
+  ## Usage 
+  ${info.usageInstructions}
+
+  ## Contribution
+  ${info.contributionInstructions}
+
+  ## Testing
+  ${info.testInstructions}
+
+  ## Questions
+  Reach out to the repo owner, [${github}](https://github.com/${github}) at ${info.questionsEmail}.
 `;
 }
 
